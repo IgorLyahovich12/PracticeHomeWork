9331fdd520cda876f93e783e53cd8c4606cc21e9
IgorLyahovich12 <igorlyahovich@gmail.com>
2023-11-25 13:07:55 +0200

    HomeWork19 in the course of development

--- src/main/java/edu/igor/hw19/Ex10/Ex10.java
+++ src/main/java/edu/igor/hw19/Ex10/Ex10.java
@@ -1,19 +1,25 @@
 package edu.igor.hw19.Ex10;
 
+
+
 public class Ex10 {
-    //10. Дано наступні класи
-    //class shape { /* ... */ }
-    //class Circle extends shape { /* ... */ }
-    //class Rectangle extends Shape { /* ... */ }
-    //class Node { /* ... */ }
+
+    static class Shape {
+    }
+
+    static class Circle extends Shape {
+    }
+
+    static class Rectangle extends Shape {
+    }
+
+    static class Node<T> {
+    }
+
+
     //Чи компілюватиметься наведений нижче код? Якщо ні, то чому?
-    //Node nc = new Node<>();
-    //Node ns = nc;
-    //Не скомпілюється тому , що Node не може приймати різні типи
-    //class Shape { /* ... */ }
-    //class Circle extends Shape { /* ... */ }
-    //class Rectangle extends Shape { /* ... */ }
-    //class Node<T> { /* ... */ }
+    Node< Circle> nc = new Node<>();
+    //Node<extends Shape> ns = nc;
+    }
     //Node<Circle> nc = new Node<>();
-    //Node<Shape> ns = nc;  // Тепер це буде правильно скомпільовано
-}
+    //Node<? extends Shape> ns = nc;
